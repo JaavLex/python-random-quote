a652e15bc21655656af9a47625b55e8dde3c8957
TacticsCH <alexandre.javet@hotmail.com>
2020-03-20 17:07:01 +0100

    com5

--- get-quote.py
+++ get-quote.py
@@ -5,7 +5,10 @@ def lol():
   quotes = f.readlines()
   f.close()
 
-  print(quotes[13])
+  last = len(quotes) - 1
+  rnd = random.randint(0, last)
+
+  print(quotes[rnd])
 
 if __name__== "__main__":
   lol()
